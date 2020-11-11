e9747f92479ddf7faf2b0965deca908c8917afe4
rsbqr <rsbqr.coder@gmail.com>
2020-11-12 01:33:52 +0700

    pop up tidak berjalan di node

--- j. pop-up boxes.js
+++ j. pop-up boxes.js
@@ -2,5 +2,26 @@
 menampilkan pesan di layar
 */
 
+console.log("pop up ini tidak bisa di run dengan node karena perlu scripting html")
+
 //confirm("Ini jenis popup konfirmasi (confirm)");
-prompt("ini jenis popup prompt");
+//prompt("ini jenis popup prompt");
+//alert("woi");
+
+console.log("cek line yang ada di bawah untuk menyalin script tsb kemudian save dengan ekstensi html"
+/*
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script>
+        //alert("Hello");
+        //prompt("bye!");
+        confirm("woi");
+    </script>
+</body>
+</html>) */
