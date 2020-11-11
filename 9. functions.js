8d239c41a57a2913fdcea8b0f949e836250c40b2
rsbqr <43677172+rsbqr@users.noreply.github.com>
2020-11-11 22:04:00 +0700

    basic javascript

---
+++ 9. functions.js
@@ -0,0 +1,22 @@
+/*#9 function
+Dalam JavaScript, function (fungsi) : fitur
+penting dari program, dan fungsi dapat mengakses 
+variabel lokal dari fungsi induk (closure).
+*/
+
+//fungsi temen dengan parameter nama (cara 1)
+function temen (nama){
+    //ketika fungsi di jalankan akan menampilkan ini
+    return "nama temanku " +nama;
+}
+//parameternya dimasukin langsung didalam fungi temen
+console.log(temen("Jomblo")); 
+
+//fungsi tanpa nama (anonim) dengan parameter name (cara 2)
+var salam = function(name) //namun fungsi diakses dengan variabel
+{
+    return "Halo " + name;
+}
+/*hampir sama dengan yg diatas namun 
+nama variabelnya dipakai untuk panggil parameternya*/
+console.log(salam("Mblo"));
